ed17e079a53b12061b113559059a1d1a80468b1b
Iván Andrés Lafaurie <ilafaurie@consware.com.co>
2023-11-23 17:55:50 -0500

    proyecto terminado

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,74 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+// const GIPHY_API_KEY = 'XlOF1soTQ4sshdox3tam43oqRPopNfl0'
+
+@Injectable({ providedIn: 'root'})
+export class GifsService {
+
+public gifList: Gif[]= [];
+
+  private _tagsHistory: string[] = [];
+  private apiKey: string = 'XlOF1soTQ4sshdox3tam43oqRPopNfl0';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs Service Ready');
+  }
+
+  get tagHistory() {
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory (tag: string){
+    tag = tag.toLocaleLowerCase();
+
+    if(this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void {
+    localStorage.setItem('history', JSON.stringify( this._tagsHistory ));
+  }
+
+  private loadLocalStorage():void {
+    if( !localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+
+    if ( this._tagsHistory.length === 0 ) return;
+    this.searchTag( this._tagsHistory[0] );
+  }
+
+
+   searchTag(tag: string):void{
+
+    if(tag.length == 0 ) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '20')
+    .set('q', tag)
+
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, {params})
+    .subscribe(resp=>{
+      this.gifList = resp.data;
+      console.log(resp);
+    });
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=XlOF1soTQ4sshdox3tam43oqRPopNfl0&q=valorant&limit=10')
+    // .then(resp => resp.json())
+    // .then(data => console.log(data))
+    // this._tagsHistory.unshift(tag);
+
+
+  }
+}
